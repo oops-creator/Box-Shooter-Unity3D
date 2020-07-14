e805647043e6f16dd2596f042bfaae8b84fc7c3e
oops-creator <still.shivam@gmail.com>
2020-07-15 02:18:59 +0530

    Add particles

--- Assets/Scripts/PlayAgain.cs
+++ Assets/Scripts/PlayAgain.cs
@@ -6,9 +6,11 @@ public class PlayAgain : MonoBehaviour {
 	// respond on collisions
 	void OnCollisionEnter(Collision newCollision)
 	{
+		
 		// only do stuff if hit by a projectile
 		if (newCollision.gameObject.tag == "Projectile") {
 			// call the RestartGame function in the game manager
+		
 			GameManager.gm.RestartGame();
 		}
 	}
